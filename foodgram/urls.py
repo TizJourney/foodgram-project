ecc0e26235ce6b6cabd388355e001a967aaf3d42
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-21 18:01:43 +0300

    Сделать первую версию ручки signup

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
+    path('auth/', include('users.urls')),        
 ]
 
 if settings.DEBUG:
