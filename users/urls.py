0920ef044cf6e6198992294db53d527e6432824f
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-21 18:21:06 +0300

    Базовые версии для сброса пароля

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
-    path('login/', views.Login.as_view(), name='login')
+    path('login/', views.Login.as_view(), name='login'),
+    path('change/', views.ChangePassword.as_view(), name='change_password'),
+    path('reset/', views.ResetPassword.as_view(), name='reset_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -9,7 +9,20 @@ class Register(CreateView):
     success_url = reverse_lazy('ua')
     template_name = 'users/reg.html'
 
+
 class Login(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('ua')
     template_name = 'users/authForm.html'
+
+
+class ChangePassword(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('ua')
+    template_name = 'users/changePassword.html'
+
+
+class ResetPassword(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('ua')
+    template_name = 'users/resetPassword.html'
