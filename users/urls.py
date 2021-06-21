866bf8bf34472fb8362a8d26fdea948a0fa6c9ae
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-21 18:16:40 +0300

    сделать страницы регистраци и логина

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup')
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.Login.as_view(), name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,12 @@ from django.views.generic import CreateView
 from .forms import CreationForm
 
 
-class SignUp(CreateView):
+class Register(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('ua')
+    template_name = 'users/reg.html'
+
+class Login(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('ua')
     template_name = 'users/authForm.html'
