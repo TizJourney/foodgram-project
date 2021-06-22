24cdfc5d8f3588525101c01f2d713648f31506b8
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-22 23:20:12 +0300

    view для создания рецепта

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import handler404, handler500  # noqa
 from django.urls import path
 
-from rest_framework.routers import DefaultRouter
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new_recipe/', views.new_recipe, name='new_recipe'),
+    path('new_recipe/', views.NewRecipe.as_view(), name='new_recipe'),
     # path('follow/', views.follow_index, name='follow_index'),
 #     path('group/<slug:slug>/', views.group_posts, name='group'),
 
