cf1763f1f1bf4ea4d17a617f6d342528eafd758d
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-22 19:38:13 +0300

    первая версия формы для авторизаций

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,29 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class CreationForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "username", "email")
+
+
+# from django import forms
+
+# from .models import Comment, Post
+
+
+# class PostForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Post
+#         fields = ('text', 'group', 'image')
+
+
+# class CommentForm(forms.ModelForm):
+
+#     class Meta:
+#         model = Comment
+#         fields = ('text',)
+#         widgets = {
+#             'text': forms.Textarea()
+#         }
--- users/views.py
+++ users/views.py
@@ -1,28 +1,29 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.edit import FormView, UpdateView
 
-from .forms import CreationForm
+from .forms import RegisterForm
 
 
 class Register(CreateView):
-    form_class = CreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy('ua')
     template_name = 'users/reg.html'
 
 
 class Login(CreateView):
-    form_class = CreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy('ua')
     template_name = 'users/authForm.html'
 
 
 class ChangePassword(CreateView):
-    form_class = CreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy('ua')
     template_name = 'users/changePassword.html'
 
 
 class ResetPassword(CreateView):
-    form_class = CreationForm
+    form_class = RegisterForm
     success_url = reverse_lazy('ua')
     template_name = 'users/resetPassword.html'
