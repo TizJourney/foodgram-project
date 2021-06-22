a6a27773106888ffc0d8dd4055213ddde25ae4bf
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-23 01:18:05 +0300

    поменять префикс для авторизаций

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('users.urls')),    
+    path('accounts/', include('users.urls')),    
     path('', include('web.urls')),
 ]
 
