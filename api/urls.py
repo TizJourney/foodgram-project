9b89f2e5db7e4a99a086962c8c4c9a81233782dd
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-25 01:28:29 +0300

    Реализовать функциональность по удалению

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('favorites/', views.FavoritesView.as_view(), name='favorites'),
+    path('favorites/<int:recipe_id>/', views.favorite_delete, name='favorites_delete'),
 ]
