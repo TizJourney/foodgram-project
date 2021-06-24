d3d9010817dd4b29cae7196c59c43a3edefe1bf1
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-24 19:32:45 +0300

    добавить сортировку

--- web/models.py
+++ web/models.py
@@ -80,6 +80,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+        ordering = ('-pub_date',)
 
     def __str__(self):
         title = f'Рецепт {self.name}'
