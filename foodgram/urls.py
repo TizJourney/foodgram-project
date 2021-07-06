11451e84dc6b6146f17274cd7c0f5bc61fc30cae
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-06 22:42:57 +0300

    ещё исправление стилей

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import handler404, handler500 #noqa
+from django.conf.urls import handler404, handler500  # noqa
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -11,16 +11,15 @@ urlpatterns = [
     path('', include('web.urls')),
 ]
 
-handler404 = 'web.views.page_not_found' #noqa
-handler500 = 'web.views.server_error' #noqa
+handler404 = 'web.views.page_not_found'  # noqa
+handler500 = 'web.views.server_error'  # noqa
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-        )
+    )
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
-        )    
+    )
     urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
-
