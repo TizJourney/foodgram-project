c16da009bdfec408d4492a677e108a5cdf72041f
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-06 18:11:44 +0300

    Дебажная посылка почты

--- foodgram/settings_local.py
+++ foodgram/settings_local.py
@@ -40,3 +40,6 @@ DEBUG_TOOLBAR_PANELS = [
 INTERNAL_IPS = [
 '127.0.0.1',
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
