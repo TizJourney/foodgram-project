c381d618ab57426c4555a118868266559924895c
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-11 23:02:46 +0300

    обновить настройки

--- foodgram/settings_common.py
+++ foodgram/settings_common.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'sorl.thumbnail',
     'web',
     'users',
