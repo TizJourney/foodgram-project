6327a7e59a528d24423898be2003470dd2c823ea
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-11 22:56:22 +0300

    новые настройки для почты

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -31,10 +31,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_FROM_EMAIL = 'admin@foodgram.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
