0d83e70365359429f20c669b4787b87de428dd3e
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-11 21:59:30 +0300

    Настройки для отправки почты в облаке

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -24,9 +24,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_FROM_EMAIL = 'admin@foodgram.com'
 
-# настройка посылки e-mail: используем "файловую" посылку. по сути эмуляция
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS=True
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
