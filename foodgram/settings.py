e593b40e880ad13a034eef4830424c2afb2a9777
Konstantin Bondar <kostya.bondar@gmail.com>
2021-07-11 21:19:45 +0300

    поменять версию базы данных в облаке

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -7,8 +7,12 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
